9c29c5b34c0ed6f750f53832a27a9a0f47f3b358
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-11-28 23:06:59 +0530

    add note functionalty

--- iNotebook/src/components/Notes.js
+++ iNotebook/src/components/Notes.js
@@ -1,18 +1,22 @@
-import React,{useContext} from 'react'
-import noteContext from "../context/notes/noteContext";
+import React,{useContext} from 'react';
+import AddNote from './AddNote';
 import Noteitems from './Noteitems';
+import noteContext from "../context/notes/noteContext";
 
 const Notes = () => {
-    const context = useContext(noteContext);
-    const {notes, setNotes} = context;  
-    // eslint-disable-next-line
+  const context = useContext(noteContext);
+  const { notes, addNote } = context;
+  // eslint-disable-next-line
   return (
+    <>
+    <AddNote/>
     <div className="row my-3">
         <h1>Your Notes</h1>
         {notes.map((note)=>{
           return <Noteitems key={note._id} note={note}/>
         })}
     </div>
+    </>
   )
 }
 
--- iNotebook/src/context/notes/NoteState.js
+++ iNotebook/src/context/notes/NoteState.js
@@ -1,159 +1,55 @@
-import NoteContext from './noteContext';
-import { useState } from 'react';
+import NoteContext from "./noteContext";
+import { useState } from "react";
 
-const NoteState = (props)=>{
-   const notesinitioal = [
+const NoteState = (props) => {
+  const notesinitioal = [
     {
-        "_id": "67419ef93e42634626ac5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
+      _id: "67419ef9e42634626ac5e1c1",
+      user: "673f97d4aaaa2a06cbc02ae3",
+      title: "My Java",
+      description: "Java is a most Popular laungage",
+      tag: "Public",
+      date: "2024-11-23T09:23:05.236Z",
+      __v: 0,
     },
     {
-        "_id": "67419efce426334626ac5e1c3",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:08.195Z",
-        "__v": 0
+      _id: "67419efce42634626ac5e1c3",
+      user: "673f97d4aaaa2a06cbc02ae3",
+      title: "My Java",
+      description: "Java is a most Popular laungage",
+      tag: "Public",
+      date: "2024-11-23T09:23:08.195Z",
+      __v: 0,
     },
-    {
-        "_id": "67419ef9e42634626ac35e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419ef9e42634626ac53e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "673419ef9e42634626ac5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "674129ef9e42634626ac5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419ef9e426346226ac5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419ef9e42634626ac5e21c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419ef9e42634626adc5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419ef9e42634626dac5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419efd9e426346h26ac5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419efd9e42634626ac5em1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419sef9e42634626ac5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "6741s9ef9e42634626ac5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419ef9e42634626abc5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-    {
-        "_id": "67419ef9e4263b4626ac5e1c1",
-        "user": "673f97d4aaaa2a06cbc02ae3",
-        "title": "My Java",
-        "description": "Java is a most Popular laungage",
-        "tag": "Public",
-        "date": "2024-11-23T09:23:05.236Z",
-        "__v": 0
-    },
-   ]
-   const [notes, setNotes] = useState(notesinitioal)
-    return (
-        <NoteContext.Provider value={{notes, setNotes}}>
-            {props.children}
-        </NoteContext.Provider>
-    )
-}
+  ];
+  const [notes, setNotes] = useState(notesinitioal);
+
+  // Add Notes
+  const addNote = (title, description, tag) => {
+    // todo: API Call
+    console.log("Adding a new Note")
+    const note = {
+      _id: "67419ef9e4263462e6ac5e1c1",
+      user: "673f97d4aaaa2a06cbcs02ae3",
+      title: title,
+      description: description,
+      tag: tag,
+      date: "2024-11-23T09:23:05.236Z",
+      __v: 0,
+    };
+    setNotes(notes.concat(note));
+  };
+  // Update Notes
+  const updateNote = () => {};
+
+  // Delete Notes
+  const deleteNote = () => {};
+
+  return (
+    <NoteContext.Provider value={{ notes, addNote, updateNote, deleteNote }}>
+      {props.children}
+    </NoteContext.Provider>
+  );
+};
 
 export default NoteState;
