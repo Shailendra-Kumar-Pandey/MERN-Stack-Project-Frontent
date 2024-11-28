a44d2ba02c5831cb51e8a07033e33bf7a7b32331
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-11-28 16:29:12 +0530

    feach all notes api with backend

--- iNotebook/src/context/notes/NoteState.js
+++ iNotebook/src/context/notes/NoteState.js
@@ -1,22 +1,30 @@
 import NoteContext from './noteContext';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 const NoteState = (props)=>{
-    // const s1 = {
-    //     "name": "Jeetu Bhai",
-    //     "class": "12th"
-    // }
-    // const [state, setState] = useState(s1);
-    // const update = ()=>{
-    //     setTimeout(() => {
-    //         setState({
-    //             "name": "Shailendra Kumar Pandey",
-    //             "class": "MBA"
-    //         })
-    //     }, 3000);
-    // }
+   const notesinitioal = [
+    {
+        "_id": "67419ef9e42634626ac5e1c1",
+        "user": "673f97d4aaaa2a06cbc02ae3",
+        "title": "My Java",
+        "description": "Java is a most Popular laungage",
+        "tag": "Public",
+        "date": "2024-11-23T09:23:05.236Z",
+        "__v": 0
+    },
+    {
+        "_id": "67419efce42634626ac5e1c3",
+        "user": "673f97d4aaaa2a06cbc02ae3",
+        "title": "My Java",
+        "description": "Java is a most Popular laungage",
+        "tag": "Public",
+        "date": "2024-11-23T09:23:08.195Z",
+        "__v": 0
+    }
+   ]
+   const [notes, setNotes] = useState(notesinitioal)
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
