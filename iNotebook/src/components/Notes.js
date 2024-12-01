062eaf7997bd846842efacc52a39608c1c6be78b
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-12-01 14:36:51 +0530

    client side add edit node modal and function

--- iNotebook/src/components/Notes.js
+++ iNotebook/src/components/Notes.js
@@ -1,27 +1,132 @@
-import React,{useContext} from 'react';
-import AddNote from './AddNote';
-import Noteitems from './Noteitems';
+import React, { useContext, useEffect, useRef, useState } from "react";
+import AddNote from "./AddNote";
+import Noteitems from "./Noteitems";
 import noteContext from "../context/notes/noteContext";
-import { useEffect } from 'react';
 
 const Notes = () => {
+  const[note, setNote]=useState({etitle:"", edescription:"",etag:""});
   const context = useContext(noteContext);
-  const { notes, getNotes} = context;
-  useEffect(()=>{
+  const { notes, getNotes } = context;
+  useEffect(() => {
     getNotes();
     // eslint-disable-next-line
-  },[]);
+  }, []);
+  const ref = useRef("");
+  const updateNote = (currentNote) => {
+    ref.current.click();
+    setNote({etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag});
+  };
+  const handleClick = (e)=>{
+    console.log("Updating the note...", note )
+    e.preventDefault();
+
+    // addNote(note.title, note.description, note.tag);
+  }
+  const onChange = (e)=>{
+    setNote({...note, [e.target.name]: e.target.value})
+  }
+
   return (
     <>
-    <AddNote/>
-    <div className="row my-3">
+      <AddNote />
+      <button
+        ref={ref}
+        type="button"
+        className="btn btn-primary d-none"
+        data-bs-toggle="modal"
+        data-bs-target="#exampleModal"
+      >
+        Launch demo modal
+      </button>
+      <div
+        className="modal fade"
+        id="exampleModal"
+        tabIndex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5" id="exampleModalLabel">
+                Edit title
+              </h1>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+          <form className= "my-3">
+            <div className="modal-body">
+              <div className="mb-3">
+                <label htmlFor="etitle" className="form-label">
+                  Title
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="etitle"
+                  name="etitle"
+                  aria-describedby="emailHelp"
+                  value={note.etitle}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="edescription" className="form-label">
+                  Description
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="edescription"
+                  name="edescription"
+                  value={note.edescription}
+                  onChange={onChange}
+                />
+              </div>
+              <div className="mb-3">
+                <label htmlFor="etag" className="form-label">
+                  Tag
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="etag"
+                  name="etag"
+                  value={note.etag}
+                  onChange={onChange}
+                />
+              </div>
+            </div>
+          </form>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Close
+              </button>
+              <button onClick={handleClick} type="button" className="btn btn-primary">
+                Update Note
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row my-3">
         <h1>Your Notes</h1>
-        {notes.map((note)=>{
-          return <Noteitems key={note._id} note={note}/>
+        {notes.map((note) => {
+          return (
+            <Noteitems key={note._id} updateNote={updateNote} note={note} />
+          );
         })}
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default Notes;
