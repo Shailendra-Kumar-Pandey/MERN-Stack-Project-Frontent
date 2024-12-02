ca167faaed44254c8cd3d3306633f690cd9ef04e
Shailendra-Kumar-Pandey <shailendrakr2010@gmail.com>
2024-12-02 19:51:28 +0530

    client side create user with API call db

--- iNotebook/src/components/Signup.js
+++ iNotebook/src/components/Signup.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
-   
+  const [credentails, setCredentails]=useState({name:"",email:"",username:"", password:"", cpassword:""})
+  const navigate = useNavigate();
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const response = await fetch("http://localhost:5000/api/auth/createuser", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({name: credentails.name,email: credentails.email,username: credentails.username, password: credentails.password}),
+  });
+  const json = await response.json();
+  console.log(json);
+      //Save the auth token and redirect
+      localStorage.setItem('token', json.authtoken);
+      navigate('/home');
+};
+
+const onChange = (e) => {
+  setCredentails({ ...credentails, [e.target.name]: e.target.value });
+};
   return (
-    <div>
-      <h1>this is a Signup</h1>
+    <div className='container'>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label" >name</label>
+          <input type="text" className="form-control" id="name"name="name" aria-describedby="emailHelp" placeholder="Raghav Mishra"value={credentails.name} onChange={onChange}/>
+          <small id="emailHelp" className="form-text text-muted"> We'll never share your email with anyone else.</small>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label" >Email address</label>
+          <input type="email" className="form-control" id="email"  name="email" aria-describedby="emailHelp" placeholder="Enter email" value={credentails.email} onChange={onChange}/>
+          <small id="emailHelp" className="form-text text-muted"> We'll never share your email with anyone else.</small>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label" >User Name</label>
+          <input type="text" className="form-control" id="username" name="username" placeholder="Raghav123"value={credentails.username} onChange={onChange}/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label" >Password</label>
+          <input type="password" className="form-control" id="password" name="password" placeholder="Password" value={credentails.password} onChange={onChange} minLength={5} required/>
+        </div>
+        <div className="mb-3">
+          <label htmlFor="cpassword" className="form-label" >Confirm Password</label>
+          <input type="password" className="form-control" id="cpassword" name="cpassword" placeholder="Confirm Password" onChange={onChange} minLength={5} required/>
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     </div>
   )
 }
